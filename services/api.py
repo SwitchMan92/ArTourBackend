dbd48bea878589e4ee1eb484192da511f3d87a36
ng6fd11 <tom.langfeldt.external@airbus.com>
2018-07-19 17:28:09 +0200

    reworking api to be restful with flask_restful python module
    Reworked GoogleApi request to only display monuments, city halls, museums and cemeteries
    
    Reworked project architecture and added gitignore
    
    ignoring pycharm files
    
    Adding pyc files to gitignore
    
    Reworked google places function return
    
    removing google api key since bots are now using it and retrieved it from the public github...
    
    Adding config to gitignore in order to preserve api keys to be publicly displayed
    
    Removing API_KEY parameter from api module
    
    Minor changes in gitignore fileé

---
+++ services/api.py
@@ -0,0 +1,40 @@
+import wikipedia
+from config import API_KEY
+from flask import Flask
+from flask_restful import Resource, Api
+from googleplaces import GooglePlaces, types
+
+
+app = Flask(__name__)
+
+google_places = GooglePlaces(API_KEY)
+wikipedia.set_lang("fr")
+
+api = Api(app)
+
+
+class Wikipedia(Resource):
+    def get(self, wiki_name):
+        result = wikipedia.search(wiki_name)
+        result = wikipedia.page(result[0])
+        return {"result": result.content}
+
+
+class GoogleApi(Resource):
+    def get(self, longitude, latitude):
+        query_result = google_places.nearby_search(
+            lat_lng={"lat": latitude, "lng": longitude},
+            types=[types.TYPE_PREMISE, types.TYPE_SUBPREMISE, types.TYPE_CITY_HALL, types.TYPE_MUSEUM,
+                   types.TYPE_NATURAL_FEATURE, types.TYPE_CEMETERY]
+            )
+
+        data = list()
+
+        for result in query_result.places:
+            data.append({
+                    "name": result.name,
+                    "details": result.get_details(),
+                    "latitude": float(result.geo_location["lat"]),
+                    "longitude": float(result.geo_location["lng"])
+                })
+        return data
